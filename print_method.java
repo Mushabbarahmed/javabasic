b10a449b55d75e9c65f15f9f0fc86f8469a22382
mushabbar ahmed <90035278+Mushabbarahmed@users.noreply.github.com>
2023-05-21 15:10:26 +0530

    Add files via upload
    Different print methods in java.

---
+++ print_method.java
@@ -0,0 +1,15 @@
+public class print_method {
+
+    public static void main(String[] args) {
+
+        System.out.print("hasghs");
+        System.out.println("hasghs");
+        int a =10,b=11;
+        float d1=2.000f;
+
+
+        System.out.printf("thevalue of a is %d and the value of b is : %d",a,b);
+        System.out.print("\n");
+        System.out.format("thevalue of a is %d and the value of b is : %8.2f",a,d1);
+    }
+}
