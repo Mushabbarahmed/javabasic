734e7f5be52dfb439a5ef9994629afec448c4558
mushabbar ahmed <90035278+Mushabbarahmed@users.noreply.github.com>
2023-05-21 14:58:43 +0530

    Add files via upload
    Different operators.

---
+++ operators.java
@@ -0,0 +1,24 @@
+
+public class operators {
+    public static void main(String[] args) {
+
+        // arithematic operator
+        int a=10;
+        int b=9;
+        int sum= a + b;
+
+        // comaparison operator
+        System.out.println(a<b);
+        System.out.println(a>b);
+
+        //logical operator
+        System.out.println(a >b && a<b);
+
+        //assigment operator
+        int a1 = 88;
+
+        // bitwise operator
+        System
+                .out.println(21&3);
+        }
+}
