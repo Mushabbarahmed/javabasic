ef7688916b6af7a7a39e32f4377f4d47924be809
mushabbar ahmed <90035278+Mushabbarahmed@users.noreply.github.com>
2023-05-21 15:09:25 +0530

    Add files via upload

---
+++ datatypenincdecrement.java
@@ -0,0 +1,11 @@
+public class datatypenincdecrement {
+
+    public static void main(String[] args) {
+
+        int j=58;
+        System.out.println(j++);
+        int a =j;
+        System.out.println(a);
+        System.out.println(++j);
+    }
+}
