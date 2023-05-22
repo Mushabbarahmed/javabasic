a4f06d72696cb9c572c90b42ac4abae15213f699
mushabbar ahmed <90035278+Mushabbarahmed@users.noreply.github.com>
2023-05-22 21:42:47 +0530

    if else (control statements)

---
+++ if_else.java
@@ -0,0 +1,20 @@
+public class if_else {
+    public static void main(String[] args)
+    {
+        if (1>3)
+        {
+            System.out.println("1 greater than 3");
+        }
+        else
+        {
+            System.out.println("1 is not greater thann 3");
+        }
+        if (10>1 && 1<10)
+        {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
