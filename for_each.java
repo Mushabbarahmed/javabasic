e20266a6ba94ec44b347722cd55834dc26def70c
Mushabbar ahmed <90035278+Mushabbarahmed@users.noreply.github.com>
2024-05-21 22:55:03 +0530

    Add files via upload

---
+++ for_each.java
@@ -0,0 +1,14 @@
+public class for_each{
+
+    public static void main(String[] args) {
+        int [] mark1=new int[2];
+        int [] marks={1 ,12,12,121,1};
+//        for each in array
+        for( int ele:marks)
+        {
+            System.out.println(ele);
+        }
+
+
+    }
+}
---
+++ for_loop.java
@@ -0,0 +1,16 @@
+public class for_loop {
+
+    public static void main(String[] args) {
+        System.out.println("hasghs");
+        int n=5;
+        for (int i=0;i<=n;i++)
+
+        {
+            System.out.println(i);
+        }
+        for (int j=7;j!=-1;j--)
+        {
+            System.out.println(j);
+        }
+    }
+}
